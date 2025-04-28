f87960e6be72c00dceb75bb9c44613d4d0b7de70
Simrit Garg <simritgarg@gmail.com>
2025-04-28 17:00:57 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.java
+++ Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.java
@@ -9,25 +9,22 @@ public class GFG {
         int testCases = Integer.parseInt(br.readLine());
 
         while (testCases-- > 0) {
-            // Read capacity
             int capacity = Integer.parseInt(br.readLine());
-
-            // Read values
             String[] valInput = br.readLine().split(" ");
+            String[] wtInput = br.readLine().split(" ");
+
             int[] val = new int[valInput.length];
+            int[] wt = new int[wtInput.length];
+
             for (int i = 0; i < valInput.length; i++) {
                 val[i] = Integer.parseInt(valInput[i]);
             }
 
-            // Read weights
-            String[] wtInput = br.readLine().split(" ");
-            int[] wt = new int[wtInput.length];
             for (int i = 0; i < wtInput.length; i++) {
                 wt[i] = Integer.parseInt(wtInput[i]);
             }
 
-            // Call the knapSack function and print the result
-            System.out.println(Solution.knapSack(capacity, val, wt));
+            System.out.println(Solution.knapsack(capacity, val, wt));
             System.out.println("~");
         }
     }
@@ -35,30 +32,28 @@ public class GFG {
 
 // } Driver Code Ends
 
+
 class Solution {
-    // Function to return max value that can be put in knapsack of capacity.
-    static int knapSack(int capacity, int val[], int wt[]) {
-        int n = wt.length;
-        int[][] dp = new int[n+1][capacity+1];
-        for(int i=0;i<=wt.length;i++){
-            for(int j=0;j<=capacity;j++){
+    static int knapsack(int W, int val[], int wt[]) {
+        int[][] dp = new int[wt.length+1][W+1];
+        // for(int[] i: dp){
+        //     Arrays.fill(i,-1);
+        // }
+        for(int i=0;i<dp.length;i++){
+            for(int j=0;j<dp[0].length;j++){
                 dp[i][j] = -1;
             }
         }
-        return ways(capacity,0,val,wt,dp);
+        return solve(W,0,val,wt,dp);
     }
-    static int ways(int capacity, int idx, int val[], int wt[], int[][] dp){
-        if(capacity == 0) return 0;
-        if(idx == wt.length) return 0;
-        if(dp[idx][capacity]!=-1) return dp[idx][capacity];
+    static int solve(int W, int idx, int[] val, int[] wt, int[][] dp){
+        if(idx==wt.length) return 0;
+        if(dp[idx][W] != -1) return dp[idx][W];
         int pick = 0;
-        if(wt[idx]<=capacity){
-            pick = val[idx] + ways(capacity-wt[idx],idx+1,val,wt,dp);
+        if(W>=wt[idx]){
+            pick = val[idx] + solve(W-wt[idx],idx+1,val,wt,dp);
         }
-        int skip = ways(capacity,idx+1,val,wt,dp);
-        dp[idx][capacity] = Math.max(pick,skip);
-        return dp[idx][capacity];
-        
-        
+        int skip =solve(W,idx+1,val,wt,dp);
+        return dp[idx][W] = Math.max(pick,skip);
     }
 }
